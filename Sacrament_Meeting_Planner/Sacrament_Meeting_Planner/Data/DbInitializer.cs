ee38f1479ecabe96164a4174c444f49d90237e78
jambrizz <ambrizjovani@gmail.com>
2023-12-05 20:49:49 -0800

    Sort out configuration exception

---
+++ Sacrament_Meeting_Planner/Sacrament_Meeting_Planner/Data/DbInitializer.cs
@@ -0,0 +1,82 @@
+﻿using Sacrament_Meeting_Planner.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sacrament_Meeting_Planner.Data
+
+{
+
+    public static class DbInitializer
+
+    {
+
+        public static void Initialize(SacramentContext context)
+
+        {
+
+            context.Database.EnsureCreated();
+
+
+
+            if (context.Meetings.Any())
+
+            {
+
+                return;
+
+            }
+
+            var meetings = new Meeting[]
+
+            {
+
+                new Meeting
+
+                {
+
+                    DateOfMeeting = DateTime.Parse("2023-01-01"),
+
+                    Leader = "John Doe",
+
+                    OpeningHymn = "123",
+
+                    SacramentHymn = "456",
+
+                    ClosingHymn = "789",
+
+                    IntermediateNumber = "321",
+
+                    OpeningPrayer = "Jane Doe",
+
+                    ClosingPrayer = "Bob Smith",
+
+                    MeetingSpeakers = new List<Speaker>
+
+                    {
+
+                        new Speaker { SpeakerName = "Speaker 1", Topic = "Topic 1", SpeakerPosition = 1 },
+
+                        new Speaker { SpeakerName = "Speaker 2", Topic = "Topic 2", SpeakerPosition = 2 }
+
+                    }
+
+                },
+
+            };
+
+            foreach (Meeting m in meetings)
+
+            {
+
+                context.Meetings.Add(m);
+
+            }
+
+            context.SaveChanges();
+
+        }
+
+    }
+
+}
