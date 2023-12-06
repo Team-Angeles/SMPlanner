1906789e8159707c7e9d86c94cfca687e09d162c
jambrizz <ambrizjovani@gmail.com>
2023-12-05 19:41:24 -0800

    Initial MVC creation

---
+++ Sacrament_Meeting_Planner/Sacrament_Meeting_Planner/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sacrament_Meeting_Planner.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
