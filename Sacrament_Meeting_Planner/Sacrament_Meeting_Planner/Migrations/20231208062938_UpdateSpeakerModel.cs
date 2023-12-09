dbbc0c71d6a35e57265b6fd791d1eaf5655ff1af
sarahsteadman <fairyfaye4real@gmail.com>
2023-12-08 20:22:40 -0700

    Added JAVAJAVAJAVA

---
+++ Sacrament_Meeting_Planner/Sacrament_Meeting_Planner/Migrations/20231208062938_UpdateSpeakerModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sacrament_Meeting_Planner.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSpeakerModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Meeting",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateOfMeeting = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Leader = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OpeningHymn = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SacramentHymn = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ClosingHymn = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IntermediateNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OpeningPrayer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ClosingPrayer = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Meeting", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Speaker",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MeetingId = table.Column<int>(type: "int", nullable: false),
+                    Topic = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SpeakerPosition = table.Column<int>(type: "int", nullable: false),
+                    SpeakerName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Speaker", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Speaker_Meeting_MeetingId",
+                        column: x => x.MeetingId,
+                        principalTable: "Meeting",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Speaker_MeetingId",
+                table: "Speaker",
+                column: "MeetingId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Speaker");
+
+            migrationBuilder.DropTable(
+                name: "Meeting");
+        }
+    }
+}
